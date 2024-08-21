aaa0fb3e8bd0269e9a421b496cd085f1d9d062da
Chriswiz13908 <62439031+Chriswiz13908@users.noreply.github.com>
2024-08-21 00:04:38 -0400

    Add files via upload

---
+++ Day_1/variable.js
@@ -0,0 +1,21 @@
+// Declaring different variables of different data types
+
+let firstName = "Christopher"
+let lastName = "Wolverton"
+let country = "USA"
+let city = "Beckley"
+let age = "28"
+let isMarried = "False"
+
+// Declaring variables with number values
+
+const gravity = 9.81            // earth gravity in m/s2
+const boilingPoint = 100        // water boiling point, temp in oC
+const PI = 3.14                 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = "Christopher",
+    job = "chef",
+    live  = "USA"
+    
